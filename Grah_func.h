856d993f6cecca043500b3f9dc73753874929f83
yuyi1111 <45939227+yuyi1111@users.noreply.github.com>
2018-12-20 21:39:39 +0800

    完成了Find_attractions
    完成了景点查询

--- Grah_func.h
+++ Grah_func.h
@@ -3,6 +3,6 @@
 void CreateGraph(AdjGraph *G, int A[MAXV][MAXV], int v, int e);
 //void DispAdj(AdjGraph *G);
 void DestoryAd(AdjGraph* G);
-void Find_attractions(int num);
-void Find_road(int v1, int v2);
+void Find_attractions(AdjGraph* G,int num);
+void Find_road(AdjGraph* G,int v1, int v2);
 void Recommend(int num);//输入景点
