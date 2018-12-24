8a2e9a2ef7cf68ac23a68380d6434c577005687d
Chuhan <zch814801245@hotmail.com>
2018-12-24 17:15:29 +0800

    实现道路查询
    添加Find_road、Floyd、Dispath

--- Grah_func.h
+++ Grah_func.h
@@ -6,3 +6,6 @@ void DestoryAd(AdjGraph* G);
 void Find_attractions(AdjGraph* G,int num);
 void Find_road(AdjGraph* G,int v1, int v2);
 void Recommend(int num);//输入景点
+int LocateVex(AdjGraph* G,int u);   //输入景点编号
+void Floyd(int g[MAXV][MAXV],int v1, int v2);
+void Dispath(int g[][MAXV], int A[][MAXV], int path[][MAXV], int i, int j);
